70c426ced67ab935faf1909b75bb120ad5ea3f21
Matheus Ferreira Frederico <maathe.f@gmail.com>
2017-08-08 01:25:49 -0400

    Adjust routes
    Controller especification in routers.js

--- docs/js/routes.js
+++ docs/js/routes.js
@@ -15,17 +15,17 @@ moduleApp.config(function($stateProvider, $urlRouterProvider) {
     .state('index', {
       url: "/index",
       views: {
-        'Home': { templateUrl: '../templates/home.html'},
-        'Plans': { templateUrl: '../templates/plans.html'},
-        'Contact': { templateUrl: '../templates/contact.html'},
+        'Home': { templateUrl: '../templates/home.html', controller: 'homeCtrl'},
+        'Plans': { templateUrl: '../templates/plans.html', controller: 'plansCtrl'},
+        'Contact': { templateUrl: '../templates/contact.html', controller: 'contactCtrl'},
         'Footer':{ templateUrl: '../templates/footer.html'}
       }
     })    
     .state('faq', {
       url: "/faq",
       views: {
-        'Faq': { templateUrl: '../templates/faq.html'},
-        'Contact': { templateUrl: '../templates/contact.html'},
+        'Faq': { templateUrl: '../templates/faq.html',controller: 'faqCtrl'},
+        'Contact': { templateUrl: '../templates/contact.html',controller: 'contactCtrl'},
         'Footer':{ templateUrl: '../templates/footer.html'}
       }
     }) 
