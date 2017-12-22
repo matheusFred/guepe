65da0d28f43cb7b3902297c081e73352b795167f
Matheus Ferreira Frederico <maathe.f@gmail.com>
2017-12-22 00:56:54 -0500

    Mudanças
    Para adequar flyer

--- docs/js/controller/plansCtrl.js
+++ docs/js/controller/plansCtrl.js
@@ -9,23 +9,20 @@
 moduleApp.controller('plansCtrl', function($scope, $location, $http){
 
 	$scope.prices = [
-	    {name:'Plan Basic', value:" 200", details: [{name: "Design I",option: 1},
+	    {name:'Plan Basic', value:" 400", details: [{name: "Design I",option: 1},
 	    										   {name: "Design II",option: 0},
-	    										   {name: "Maintenance",option: 3, number: "3"},
-	    										   {name: "Helpdesk", option: 0},
-	    										   {name: "Systems",option: 3, number: "0"}
+	    										   {name: "Maintenance",option: 2, number: "3"},
+	    										   {name: "Helpdesk", option: 1}	    										   
 	    ]},
-	    {name:'Plan Dynamic', value:" 400", details: [{name: "Design I",option: 1},
+	    {name:'Plan Dynamic', value:" 600", details: [{name: "Design I",option: 1},
 	    										   {name: "Design II",option: 1},
-	    										   {name: "Maintenance",option: 3, number: "5"},
-	    										   {name: "Helpdesk", option: 0},
-	    										   {name: "Systems",option: 3, number: "0"}
+	    										   {name: "Maintenance",option: 2, number: "5"},
+	    										   {name: "Helpdesk", option: 1}
 	    ]},
-	    {name:'Plan Ultra', value:" 600", details: [{name: "Design I",option: 1},
+	    {name:'Plan Ultra', value:" 800", details: [{name: "Design I",option: 1},
 	    										   {name: "Design II",option: 1},
-	    										   {name: "Maintenance",option: 3, number: "7"},
-	    										   {name: "Helpdesk", option: 1},
-	    										   {name: "Systems",option: 3, number: "1"}
+	    										   {name: "Maintenance",option: 2, number: "7"},
+	    										   {name: "Helpdesk", option: 1}
 	    ]},
     ];
 
